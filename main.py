aac504de5bf46727e8ab302a3d2e0e5c9a02a26a
Fauzan Abrar <=>
2024-11-10 23:46:39 +0800

    add detected elements

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Description: This script is a Minesweeper solver that uses OpenCV to detect the game board and the elements on it.
+
 import pyautogui
 import cv2
 import numpy as np
@@ -12,18 +14,32 @@ def capture_screen():
     return screenshot
     
 
-def solve_minesweeper(image, reference_images):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def detect_elements(screenshot, reference_images):
+    gray = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
     detected_elements = []
 
     for key, template in reference_images.items():
         res = cv2.matchTemplate(gray, cv2.cvtColor(template, cv2.COLOR_BGR2GRAY), cv2.TM_CCOEFF_NORMED)
         threshold = 0.8
         loc = np.where(res >= threshold)
+        
+        template_height, template_width = template.shape[:2]
+        detected_mask = np.zeros_like(gray, dtype=np.uint8)
+
         for pt in zip(*loc[::-1]):
-            detected_elements.append((key, pt))
-  
-    print('detect image success')
+            element_center = (pt[0] + template_width // 2, pt[1] + template_height // 2)
+            
+            # Check if the center is within an already detected region
+            if detected_mask[pt[1]:pt[1] + template_height, pt[0]:pt[0] + template_width].sum() == 0:
+                detected_elements.append((key, element_center))
+                # Mark this region as detected
+                detected_mask[pt[1]:pt[1] + template_height, pt[0]:pt[0] + template_width] = 1
+
+    return detected_elements
+    
+
+
+def solve_minesweeper(detected_elements, reference_images):
     # Create a grid to represent the game board
     grid = {}
     for element in detected_elements:
@@ -31,35 +47,61 @@ def solve_minesweeper(image, reference_images):
         grid[(x, y)] = key
 
     moves = []
-    if detected_elements:
-      print(detected_elements[0])
+    
+    # print('Closed tiles:', [pos for pos, val in grid.items() if val == 'closed'])
+    # number_tiles = [(pos, val) for pos, val in grid.items() if val.isdigit()]
+    # print('Number tiles:', number_tiles)
+
     for (x, y), value in grid.items():
         if value.isdigit():
             num = int(value)
-            neighbors = [(x + dx, y + dy) for dx in [-1, 0, 1] for dy in [-1, 0, 1] if (dx, dy) != (0, 0)]
+            tile_size = reference_images['closed'].shape[1]
+            neighbors = [(x + dx * tile_size, y + dy * tile_size) for dx in [-1, 0, 1] for dy in [-1, 0, 1] if (dx, dy) != (0, 0)]
             closed_neighbors = [n for n in neighbors if grid.get(n) == 'closed']
-            # print(neighbors)
-            print('closed neighbour', closed_neighbors)
+            
+            # detect the bomb
             if len(closed_neighbors) == num:
-                moves.extend(closed_neighbors)
+                print('closed_neighbors',closed_neighbors, 'num', num)
+                for neighbor in closed_neighbors:
+                    if neighbor not in moves:
+                        moves.append(neighbor)
+            
+        # TODO detect the bomb and flag it
+        # TODO click the non flag tile if bomb has been detected
 
-    print('moves detected', moves)
+    print('moves', moves)
 
     # if all closed do random click
-    # if not moves:
-    #   closed_tiles = [pos for pos, val in grid.items() if val == 'closed']
-    #   if closed_tiles:
-    #       random_closed_tile = closed_tiles[np.random.randint(len(closed_tiles))]
-    #       print('random click',random_closed_tile)
-    #       moves.append(random_closed_tile)
+    if not moves:
+        number_tiles = [pos for pos, val in grid.items() if val.isdigit()]
+
+        if not number_tiles:
+            closed_tiles = [pos for pos, val in grid.items() if val == 'closed']
+            if closed_tiles:
+                random_closed_tile = closed_tiles[np.random.randint(len(closed_tiles))]
+                # moves.append(random_closed_tile)
+                perform_clicks([random_closed_tile], button='left')
+                print('random click at', random_closed_tile)
+        else:
+            print('there are number tiles')
+            for (x, y) in number_tiles:
+                num = int(grid[(x, y)])
+                tile_size = reference_images['closed'].shape[1]
+                neighbors = [(x + dx * tile_size, y + dy * tile_size) for dx in [-1, 0, 1] for dy in [-1, 0, 1] if (dx, dy) != (0, 0)]
+                closed_neighbors = [n for n in neighbors if grid.get(n) == 'closed']
+                flagged_neighbors = [n for n in neighbors if grid.get(n) == 'flag']
+                if len(flagged_neighbors) == num:
+                    for neighbor in closed_neighbors:
+                        if neighbor not in moves:
+                            moves.append(neighbor)
 
     return moves
 
 
-def perform_clicks(moves):
+def perform_clicks(moves, button='right'):
     for move in moves:
         x, y = move
-        pyautogui.click(x, y)
+        pyautogui.click(x, y, button=button)
         
 
 def main():
@@ -68,7 +110,9 @@ def main():
       '1': cv2.imread('res/1.png'),
       '2': cv2.imread('res/2.png'),
       '3': cv2.imread('res/3.png'),
+      '4': cv2.imread('res/4.png'),
       'closed': cv2.imread('res/close-tile.png'),
+      'flag': cv2.imread('res/flag.png')
     }
     
     while True:
@@ -76,9 +120,10 @@ def main():
             print("Solver started")
             while not keyboard.is_pressed('b'):
                 screenshot = capture_screen()
-                moves = solve_minesweeper(screenshot, reference_images)
+                detected_elements = detect_elements(screenshot, reference_images)
+                moves = solve_minesweeper(detected_elements, reference_images)
                 perform_clicks(moves)
-                for _ in range(50):  # Check for 'b' key press every 0.1 seconds for 5 seconds
+                for _ in range(30):  # Check for 'b' key press every 0.1 seconds for 5 seconds
                     if keyboard.is_pressed('b'):
                         print("Solver stopped")
                         return
